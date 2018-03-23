512d562da76a4b99ba0b9e5aa0e8717bdd719cc7
mogulla3 <hs.iarn3@gmail.com>
2018-03-23 16:52:24 +0900

    Implement seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Create users and stores
+1.upto(100) do |n|
+  if user = User.create(name: "user#{n}")
+    puts "created user: #{user.attributes}"
+  end
+
+  if store = Store.create(name: "store#{n}")
+    puts "created store: #{store.attributes}"
+  end
+end
+
+# Create items
+price_range = 100.step(30000, 100).to_a
+Store.all.each do |store|
+  Random.rand(1..20).times do |n|
+    if item = Item.create(name: "item#{n}-#{store.name}", price: price_range.sample, store_id: store.id)
+      puts "created item of #{store.name}: #{item.attributes}"
+    end
+  end
+end
+
+# Create orders
+item_from = Item.first.id
+item_to = Item.last.id
+user_from = User.first.id
+user_to = User.last.id
+1000.times do |n|
+  item = Item.where("id = ?", Random.rand(item_from..item_to)).first
+  user = User.where("id = ?", Random.rand(user_from..user_to)).first
+  if order = Order.create(item_id: item.id, user_id: user.id, ordered_at: DateTime.now)
+    puts "created order: #{order.attributes}"
+  end
+end
