46d64a75f98df92548d7a95015e8d3d481a0f9f3
mogulla3 <hs.iarn3@gmail.com>
2018-03-29 00:58:00 +0900

    Use include_examples instead of it_behaves_like

--- spec/lib/userrank_updater_spec.rb
+++ spec/lib/userrank_updater_spec.rb
@@ -3,7 +3,7 @@ require "mogec/userrank_updater"
 
 RSpec.describe Mogec::UserRankUpdater do
   describe "#run" do
-    shared_examples "not changing user rank" do
+    shared_examples "user rank is not changed" do
       it "should not change user rank" do
         expect {
           Mogec::UserRankUpdater.new(user: user.id).run
@@ -11,7 +11,7 @@ RSpec.describe Mogec::UserRankUpdater do
       end
     end
 
-    shared_examples "changing user rank according to purchase price" do
+    shared_examples "user rank is changed according to purchase price" do
       it "should change user rank" do
         expect {
           Mogec::UserRankUpdater.new(user: user.id).run
@@ -21,54 +21,54 @@ RSpec.describe Mogec::UserRankUpdater do
 
     context "when user purchased 0 yen" do
       let(:user) { FactoryBot.create(:user) }
-      it_behaves_like "not changing user rank"
+      include_examples "user rank is not changed"
     end
 
     context "when user purchased 10000 yen" do
       let(:user) { FactoryBot.create(:user, :purchased, price: 10000) }
-      it_behaves_like "not changing user rank"
+      include_examples "user rank is not changed"
     end
 
     context "when user purchased 10001 yen" do
       let(:user) { FactoryBot.create(:user, :purchased, price: 10001) }
       let(:rank) { "bronze" }
-      it_behaves_like "changing user rank according to purchase price"
+      include_examples "user rank is changed according to purchase price"
     end
 
     context "when user purchased 30000 yen" do
       let(:user) { FactoryBot.create(:user, :purchased, price: 30000) }
       let(:rank) { "bronze" }
-      it_behaves_like "changing user rank according to purchase price"
+      include_examples "user rank is changed according to purchase price"
     end
 
     context "when user purchased 30001 yen" do
       let(:user) { FactoryBot.create(:user, :purchased, price: 30001) }
       let(:rank) { "silver" }
-      it_behaves_like "changing user rank according to purchase price"
+      include_examples "user rank is changed according to purchase price"
     end
 
     context "when user purchased 70000 yen" do
       let(:user) { FactoryBot.create(:user, :purchased, price: 70000) }
       let(:rank) { "silver" }
-      it_behaves_like "changing user rank according to purchase price"
+      include_examples "user rank is changed according to purchase price"
     end
 
     context "when user purchased 70001 yen" do
       let(:user) { FactoryBot.create(:user, :purchased, price: 70001) }
       let(:rank) { "gold" }
-      it_behaves_like "changing user rank according to purchase price"
+      include_examples "user rank is changed according to purchase price"
     end
 
     context "when user purchased 100000 yen" do
       let(:user) { FactoryBot.create(:user, :purchased, price: 100000) }
       let(:rank) { "gold" }
-      it_behaves_like "changing user rank according to purchase price"
+      include_examples "user rank is changed according to purchase price"
     end
 
     context "when user purchased 100001 yen" do
       let(:user) { FactoryBot.create(:user, :purchased, price: 100001) }
       let(:rank) { "pratinum" }
-      it_behaves_like "changing user rank according to purchase price"
+      include_examples "user rank is changed according to purchase price"
     end
   end
 end
